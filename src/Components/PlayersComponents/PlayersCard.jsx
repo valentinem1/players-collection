9ebec9e4a32aed0b4cace8483102331d1f186bd2
Valentine <valentinemaillard1@gmail.com>
2020-06-24 18:12:50 -0400

    displayed players cards and added small styling

--- src/Components/PlayersComponents/PlayersCard.jsx
+++ src/Components/PlayersComponents/PlayersCard.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import { Card, Header, Image } from 'semantic-ui-react';
+
 const PlayersCard = (props) => {
+
+    // console.log(props.player);
+    const { first_name, last_name, position, team } = props.player;
+
     return (
         <div>
-            
+            <Card className="player-card">
+                <Image className="player-image" src="https://www.mercurynews.com/wp-content/uploads/2018/09/BNG-L-WARRIORS-0925-131.jpg" alt="players picture" />
+                <Header>{first_name + last_name}</Header>
+            </Card>
         </div>
     );
 };
