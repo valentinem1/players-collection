7c1ae2573a28afdc39de09ac0eede6ec896c5c2e
Valentine <valentinemaillard1@gmail.com>
2020-07-06 12:50:33 -0400

    created table for player listing instead of cards

--- src/Components/HomeComponents/PlayersCard.jsx
+++ src/Components/HomeComponents/PlayersCard.jsx
@@ -4,42 +4,14 @@ import { Link } from "react-router-dom"
 
 import { updateLikes } from '../../Actions/playersAction';
 
-import { Card, Header, Image, Button } from 'semantic-ui-react';
-
 const PlayersCard = ({ player, updateLikes }) => {
 
-    // handle fetch for likes
-    const handleClickLikes = (evt) => {
-        evt.preventDefault();
-        fetch(`http://localhost:3000/players/${_id}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                likes: likes + 1
-            })
-        })
-        .then(r => r.json())
-        .then(playerData => {
-            updateLikes(playerData.data.player);
-        })
-    }
-
-    const { _id, first_name, last_name, image, likes } = player;
+    // const { _id, first_name, last_name, image, likes } = player;
 
     return (
-        <Link to={`/players/${_id}`} >
-            <div>
-                <Card className="player-card">
-                    <Image className="player-image" src={image} alt="players picture" />
-                    <Header className="player-header">{first_name + ' ' + last_name}</Header>
-                    <div className="player-likes-container">
-                        <Button onClick={handleClickLikes} className="player-likes-btn">{likes} <span role='img' aria-label="heart emoji">❤️</span></Button>
-                    </div>
-                </Card>
-            </div>
-        </Link>
+        <div>
+            
+        </div>
     );
 };
 
@@ -50,3 +22,22 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps, { updateLikes })(PlayersCard);
+
+// handle fetch for likes
+    // const handleClickLikes = (evt) => {
+    //     evt.preventDefault();
+    //     fetch(`http://localhost:3000/players/${_id}`, {
+    //         method: 'PATCH',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             likes: likes + 1
+    //         })
+    //     })
+    //     .then(r => r.json())
+    //     .then(playerData => {
+    //         updateLikes(playerData.data.player);
+    //     })
+    // }
+{/* <Button onClick={handleClickLikes} className="player-likes-btn">{likes} <span role='img' aria-label="heart emoji">❤️</span></Button> */}
--- src/Components/HomeComponents/PlayersContainer.jsx
+++ src/Components/HomeComponents/PlayersContainer.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router-dom';
 import { updateMetadata }  from '../../Actions/metadataAction';
-
-import { Button } from 'semantic-ui-react';
-
+import { Button, Table} from 'semantic-ui-react';
 import ProjectCard from './PlayersCard';
 
 const PlayersContainer = ({ playersState, metadataState, updateMetadata }) => {
 
     const { current_page, total_pages } = metadataState;
 
-    const playersArr =  playersState ? playersState.map(player => <ProjectCard key={player._id} player={player} />) : null
+    // const playersArr =  playersState ? playersState.map(player => <ProjectCard key={player._id} player={player} />) : null
 
     const handleForwardPagination = (evt) => {
         evt.preventDefault();
@@ -25,9 +23,22 @@ const PlayersContainer = ({ playersState, metadataState, updateMetadata }) => {
 
     return (
         <div>
-            <div className="players-container">
-                {playersArr}
-            </div>
+            <Table striped className="players-table">
+                <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell>Player</Table.HeaderCell>
+                    <Table.HeaderCell>Team</Table.HeaderCell>
+                </Table.Row>
+                </Table.Header>
+            {playersState.map(player => 
+                <Table.Body key={player._id}>
+                <Table.Row>
+                    <Table.Cell><Link to={`/players/${player._id}`}>{player.first_name + ' ' + player.last_name}</Link></Table.Cell>
+                    <Table.Cell>{player.team.full_name}</Table.Cell>
+                </Table.Row>
+                </Table.Body> )}
+            </Table>
+            
             <div className="pagination-container">
                 <div className="divagination-text"><Button hidden={current_page === 1 ? true : false} onClick={handleBackwardPagination} className="pagination-btn" role='img' aria-label="heart emoji">←</Button> Page {current_page} of {total_pages}<Button onClick={handleForwardPagination} className="pagination-btn" role='img' aria-label="heart emoji">→</Button></div>
             </div>
