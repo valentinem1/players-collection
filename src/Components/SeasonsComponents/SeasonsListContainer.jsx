6f8f21b2e70c20cc82e09735d1fb3e4f1c51a504
Valentine <valentinemaillard1@gmail.com>
2020-07-27 10:54:02 -0400

    reversed years array from recent to older years

--- src/Components/SeasonsComponents/SeasonsListContainer.jsx
+++ src/Components/SeasonsComponents/SeasonsListContainer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const SeasonsListContainer = (props) => {
 
-    let yearsArr = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020];
+    let yearsArr = [2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010];
 
     const year = yearsArr.map(year => <Link to={`seasons/${year}`} key={year}><li>{year}</li></Link>)
 
